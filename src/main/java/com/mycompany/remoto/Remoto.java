173dcd256196f7761689b42ae9bb5b42f0c92005
IkerDAM-1 <>
2023-02-08 11:06:49 +0100

    Primera carga

---
+++ src/main/java/com/mycompany/remoto/Remoto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.remoto;
+
+/**
+ *
+ * @author dam
+ */
+public class Remoto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
